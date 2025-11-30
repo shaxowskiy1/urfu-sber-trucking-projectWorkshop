4b4ed2841dc7945c9b7c459b65d80fded6134bcc
Sergey <spavelin@inbox.ru>
2025-11-30 13:13:13 +0500

    SCRUM-68 add repository

--- daily-status-update-service/src/main/java/ru/urfu/dailystatusupdateservice/repository/DailyStatusRepository.java
+++ daily-status-update-service/src/main/java/ru/urfu/dailystatusupdateservice/repository/DailyStatusRepository.java
@@ -1,7 +1,9 @@
 package ru.urfu.dailystatusupdateservice.repository;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import ru.urfu.dailystatusupdateservice.models.Order;
 
 @Repository
-public class DailyStatusRepository {
+public interface DailyStatusRepository extends JpaRepository<Order, Long> {
 }
