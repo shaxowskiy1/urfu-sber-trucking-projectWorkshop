de9e4cae96b221c352a4d2442e713dafd4d90be7
Sergey <spavelin@inbox.ru>
2025-12-03 22:20:10 +0500

    add diff 1b55c2f

--- daily-status-update-service/src/main/java/ru/urfu/dailystatusupdateservice/service/DailyStatusService.java
+++ daily-status-update-service/src/main/java/ru/urfu/dailystatusupdateservice/service/DailyStatusService.java
@@ -2,17 +2,20 @@ package ru.urfu.dailystatusupdateservice.service;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 import ru.urfu.dailystatusupdateservice.repository.DailyStatusRepository;
 
 @Service
 @RequiredArgsConstructor
 public class DailyStatusService {
 
+    private final RestTemplate restTemplate;
     private final DailyStatusRepository dailyStatusRepository;
 
     public void updateStatus() {
         //TODO запрос к датчикам
-
+        restTemplate
+                .get
         //TODO update set sql query for updating
     }
 }
